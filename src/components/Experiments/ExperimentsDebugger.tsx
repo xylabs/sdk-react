14fa2e4c4f1920b0fb642fb8e947d71e2dd59c4c
Donald Parker <donald.parker@xylabs.com>
2022-01-24 15:18:13 -0800

    sumUpVariants for the slider

--- src/components/Experiments/ExperimentsDebugger.tsx
+++ src/components/Experiments/ExperimentsDebugger.tsx
@@ -3,11 +3,19 @@ import React from 'react'
 
 import { useLocalStorage } from '../../hooks'
 import { FlexCol, FlexRow } from '../FlexBox'
-import { ExperimentsData, ExperimentsLocalStorageKey, OutcomesData, OutcomesLocalStorageKey } from './models'
+import {
+  ExperimentsData,
+  ExperimentsLocalStorageKey,
+  OutcomesData,
+  OutcomesLocalStorageKey,
+  VariantData,
+} from './models'
 
 const ExperimentsDebugger: React.FC = ({ ...props }) => {
   const [experiments] = useLocalStorage<ExperimentsData>(ExperimentsLocalStorageKey, {})
   const [outcomes, setOutcomes] = useLocalStorage<OutcomesData>(OutcomesLocalStorageKey, {})
+
+  const sumUpVariants = (items: VariantData[]) => items.reduce((acc, curr) => acc + curr.weight, 0)
 
   return (
     <FlexCol alignItems="stretch" {...props}>
@@ -15,24 +23,29 @@ const ExperimentsDebugger: React.FC = ({ ...props }) => {
         const [name, experiment] = data
         const outcome = (outcomes[name] || 0) as number
 
-        const marks =  [
-          ...experiment.variants.map(({ key, weight }) => ({ value: weight, label: key })),
-          { value: experiment.totalWeight, label: 'Max' }
+        const marks = [
+          ...experiment.variants.map(({ key, weight }, index) => ({
+            label: key,
+            value: index === 0 ? weight : sumUpVariants(experiment.variants.slice(0, index)),
+          })),
+          { label: 'Max', value: experiment.totalWeight },
         ]
 
-        return <FlexRow key={`experiment-${name}`} alignItems="stretch">
-          {name}:&nbsp;
-          <Slider
-            aria-label={`Experiment ${name}`}
-            defaultValue={outcome}
-            value={outcome}
-            marks={marks}
-            onChange={(e, value) => {
-              outcomes[name] = Array.isArray(value) ? value[0] : value
-              setOutcomes(outcomes)
-            }}
-          />
-        </FlexRow>
+        return (
+          <FlexRow key={`experiment-${name}`} alignItems="stretch">
+            {name}:&nbsp;
+            <Slider
+              aria-label={`Experiment ${name}`}
+              defaultValue={outcome}
+              value={outcome}
+              marks={marks}
+              onChange={(e, value) => {
+                outcomes[name] = Array.isArray(value) ? value[0] : value
+                setOutcomes(outcomes)
+              }}
+            />
+          </FlexRow>
+        )
       })}
     </FlexCol>
   )
--- src/components/Experiments/models/index.ts
+++ src/components/Experiments/models/index.ts
@@ -4,4 +4,5 @@ export const ExperimentsLocalStorageKey = 'experiments'
 export type OutcomesData = Record<string, number>
 
 export type ExperimentsData = Record<string, ExperimentData>
-export type ExperimentData = { totalWeight: number; variants: { weight: number; key: string }[] }
+export type ExperimentData = { totalWeight: number; variants: VariantData[] }
+export type VariantData = { weight: number; key: string }
