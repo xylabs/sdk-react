0b9453a41fceff541183ea1f0d45539f01e9df8a
Donald Parker <donald.parker@xylabs.com>
2022-03-11 10:25:33 -0800

    fix lint

--- src/lib/Tracking/Tapjoy/StandardEvents.ts
+++ src/lib/Tracking/Tapjoy/StandardEvents.ts
@@ -1,6 +1,6 @@
 import { TapjoyStandardEvent } from './StandardEvent'
 
-class TapjoyStandardEvents <T extends Record<string, unknown>> {
+class TapjoyStandardEvents<T extends Record<string, unknown>> {
   public purchase() {
     return new TapjoyStandardEvent<T>('Purchase')
   }
