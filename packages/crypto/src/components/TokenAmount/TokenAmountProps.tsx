4899992ad9cec3a733dc0f42c3c55f486b223e79
Arie Trouw <arie.trouw@xylabs.com>
2022-06-07 13:14:20 -0700

    TokenAmount now shows 0 when it should

--- packages/crypto/src/components/TokenAmount/TokenAmountProps.tsx
+++ packages/crypto/src/components/TokenAmount/TokenAmountProps.tsx
@@ -2,7 +2,7 @@ import { ButtonExProps } from '@xylabs/react-button'
 import { BigNumber } from '@xylabs/sdk-js'
 import { MouseEventHandler } from 'react'
 
-interface TokenAmountProps extends ButtonExProps {
+export interface TokenAmountProps extends ButtonExProps {
   amount?: BigNumber | null
   label?: string
   logo?: boolean
@@ -12,5 +12,3 @@ interface TokenAmountProps extends ButtonExProps {
   statusColor?: string
   onButtonClick?: MouseEventHandler<HTMLButtonElement>
 }
-
-export type { TokenAmountProps }
