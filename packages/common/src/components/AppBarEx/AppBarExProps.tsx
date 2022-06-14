848809125c45d3d4e3991cbfea4cf1193085d765
Arie Trouw <arie.trouw@xylabs.com>
2022-06-14 12:49:13 -0700

    Menu Split in AppBarEx

--- packages/common/src/components/AppBarEx/AppBarExProps.tsx
+++ packages/common/src/components/AppBarEx/AppBarExProps.tsx
@@ -5,6 +5,7 @@ interface AppBarExProps extends AppBarProps {
   container?: 'xl' | 'lg' | 'md' | 'sm' | 'xs'
   contextToolbar?: ReactElement<ToolbarProps>
   systemToolbar?: ReactElement<ToolbarProps>
+  menu?: ReactElement<ToolbarProps>
   responsive?: boolean
 }
 
