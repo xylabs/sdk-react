b732bf989a899c5a252b7f4489da8dbd188a7dad
Donald Parker <donaldparkerii@gmail.com>
2022-11-07 08:53:17 -0800

    BUG: fix StartExperiment firing multiple times

--- packages/experiments/src/hooks/useExperiments.stories.tsx
+++ packages/experiments/src/hooks/useExperiments.stories.tsx
@@ -1,17 +1,18 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { ReactNode } from 'react'
 
 import { selectVariantForExperiment, useExperiments } from './useExperiment'
 
 const View: React.FC = () => {
-  const { experimentName, selectVariant } = useExperiments('Storybook Test', [
+  const { experimentName, selectVariant } = useExperiments<ReactNode>('Storybook Test 2', [
     { name: 'a/legacy', weight: 33 },
     { name: 'b/testing', weight: 33 },
     { name: 'c/testing', weight: 33 },
   ])
   const texts = { 'a/legacy': <p>a</p>, 'b/testing': <p>b</p>, 'c/testing': <p>c</p> }
-  const test0 = selectVariantForExperiment('Storybook Test Does not exists', texts, <p>DefaultNode</p>)
+  const test0 = selectVariantForExperiment<ReactNode>('Storybook Test Does not exists', texts, <p>DefaultNode</p>)
   const test1 = selectVariant({ 'a/legacy': <p>a</p>, 'b/testing': <p>b</p>, 'c/testing': <p>c</p> }, <p>default node</p>)
-  const test2 = selectVariantForExperiment(experimentName, texts, <p>DefaultNode</p>)
+  const test2 = selectVariantForExperiment<ReactNode>(experimentName, texts, <p>DefaultNode</p>)
   return (
     <div>
       <div>{test0}</div>
