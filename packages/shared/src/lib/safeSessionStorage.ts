782c80ce493a53a57d18f84f4729c0669b598bc0
Donald Parker <donaldparkerii@gmail.com>
2023-02-01 10:33:21 -0800

    make session storage generic

--- packages/shared/src/lib/safeSessionStorage.ts
+++ packages/shared/src/lib/safeSessionStorage.ts
@@ -1,16 +1,16 @@
 import { Log } from '@xylabs/log'
 
-export const getSessionStorageObject = <T>(key: string, log?: Log) => {
-  let result = {}
+export const getSessionStorageObject = <T>(key: string, log?: Log): T => {
+  let result = {} as T
   try {
-    result = JSON.parse(sessionStorage.getItem(key) ?? '{}')
+    result = JSON.parse(sessionStorage.getItem(key) ?? '{}') as T
   } catch (e) {
     log?.error('getSessionStorageObject', e)
   }
   return result
 }
 
-export const setSessionStorageObject = <T>(key: string, value: Record<string, unknown>, log?: Log) => {
+export const setSessionStorageObject = <T>(key: string, value: T, log?: Log) => {
   try {
     sessionStorage.setItem(key, JSON.stringify(value))
   } catch (e) {
