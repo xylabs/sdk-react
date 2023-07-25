c790011106b641e01304382789a55cae2342dfc3
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-07-25 10:12:22 -0400

    call setMode with defaultTheme prop

--- packages/invertible-theme/src/CssVarsProvider/InvertibleCssVarsProvider.tsx
+++ packages/invertible-theme/src/CssVarsProvider/InvertibleCssVarsProvider.tsx
@@ -1,15 +1,33 @@
-import { Experimental_CssVarsProvider as CssVarsProvider, responsiveFontSizes } from '@mui/material'
+import { Experimental_CssVarsProvider as CssVarsProvider, responsiveFontSizes, useColorScheme } from '@mui/material'
 import type {} from '@mui/material/themeCssVarsAugmentation'
 import { WithChildren } from '@xylabs/react-shared'
+import { useEffect } from 'react'
 
-import { InvertibleCssVarsProvider } from './InvertibleCssVars'
+import { InvertibleCssVarsProvider, Mode } from './InvertibleCssVars'
+
+export const SyncMode: React.FC<{ defaultMode?: Mode }> = ({ defaultMode }) => {
+  const { setMode } = useColorScheme()
+
+  useEffect(() => {
+    if (defaultMode) setMode(defaultMode)
+  }, [defaultMode, setMode])
+
+  return <></>
+}
 
 export interface InvertibleCssVarsProviderProps extends InvertibleCssVarsProvider, WithChildren {}
 
-export const InvertableCssVarsProvider: React.FC<InvertibleCssVarsProviderProps> = ({ children, defaultMode, noResponsiveFontSizes, theme }) => {
+export const InvertableCssVarsProvider: React.FC<InvertibleCssVarsProviderProps> = ({
+  children,
+  defaultMode = 'system',
+  noResponsiveFontSizes,
+  theme,
+}) => {
   const updatedTheme = theme ? (noResponsiveFontSizes ? theme : responsiveFontSizes(theme)) : theme
+
   return (
     <CssVarsProvider theme={updatedTheme} defaultMode={defaultMode}>
+      <SyncMode {...(defaultMode ? { defaultMode } : {})} />
       {children}
     </CssVarsProvider>
   )
