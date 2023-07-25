47ffdee000b0034f0878b30d40951327a2629d0a
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-07-25 17:09:18 -0400

    lint

--- packages/select/src/components/SelectEx.tsx
+++ packages/select/src/components/SelectEx.tsx
@@ -1,12 +1,13 @@
 import { PaletteMode, Select, SelectProps, useTheme } from '@mui/material'
 import merge from 'lodash/merge'
+import React from 'react'
 
 export interface SelectExProps<T> extends SelectProps<T> {
   colorize?: 'primary' | 'secondary'
   mode?: PaletteMode
 }
 
-export const SelectEx: <T>(props: SelectExProps<T>) => JSX.Element = ({ MenuProps, mode = 'light', colorize, ...props }) => {
+export const SelectEx: <T>(props: SelectExProps<T>) => React.JSX.Element = ({ MenuProps, mode = 'light', colorize, ...props }) => {
   const theme = useTheme()
   const colorizeMenuProps = colorize ? { MenuListProps: { sx: { backgroundColor: theme.palette[colorize][mode] } } } : {}
 
