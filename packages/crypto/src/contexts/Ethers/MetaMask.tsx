a16619eacfc7d988456fc8b8e5e316fd57c893a6
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-11-16 13:18:36 -0500

    organize provider code

--- packages/crypto/src/contexts/Ethers/MetaMask.tsx
+++ packages/crypto/src/contexts/Ethers/MetaMask.tsx
@@ -2,75 +2,28 @@ import { InfuraProvider, JsonRpcSigner, Listener, Provider, Web3Provider } from 
 import { MetaMaskInpageProvider } from '@metamask/providers'
 import { EthAddress } from '@xylabs/eth-address'
 import { useAsyncEffect } from '@xylabs/react-async-effect'
-import React, { PropsWithChildren, useEffect, useState } from 'react'
+import React, { PropsWithChildren, useCallback, useEffect, useState } from 'react'
 
 import { EthersContext } from './Context'
 import { infuraKey } from './Infura'
+
+const ethereum = window.ethereum as MetaMaskInpageProvider
 
 export interface Props {
   enabled?: boolean
 }
 
-export const MetaMaskEthersLoader: React.FC<PropsWithChildren<Props>> = (props) => {
-  const { children, enabled = true } = props
-
-  const ethereum = window.ethereum as MetaMaskInpageProvider
+export const MetaMaskEthersLoader: React.FC<PropsWithChildren<Props>> = ({ children, enabled }) => {
   const [error, setError] = useState<Error>()
-  const [localAddress, setLocalAddress] = useState<EthAddress>()
   const [resetCount, setResetCount] = useState(0)
-  const [providerName, setProviderName] = useState<string>()
-
-  const connect = async () => {
-    const accounts = await walletProvider?.send('eth_requestAccounts', [])
-    // We could have multiple accounts. Check for one.
-    if (accounts.length !== 0) {
-      setLocalAddress(accounts[0])
-      console.log('Connected: ', accounts[0])
-    } else {
-      console.log('No authorized account found.')
-    }
-    return accounts
-  }
 
   const [isConnected, setIsConnected] = useState<boolean>()
 
-  const [walletProvider, setWalletProvider] = useState<Web3Provider | null>()
+  // Provider/Signer
   const [provider, setProvider] = useState<Provider>()
+  const [walletProvider, setWalletProvider] = useState<Web3Provider | null>()
+  const [providerName, setProviderName] = useState<string>()
   const [signer, setSigner] = useState<JsonRpcSigner | null>()
-
-  useEffect(() => {
-    const accountsChangedListener: Listener = (accounts: string[]) => {
-      setResetCount(resetCount + 1)
-      setError(undefined)
-      if (accounts.length > 0) {
-        setLocalAddress(EthAddress.fromString(accounts[0]))
-      } else {
-        setLocalAddress(undefined)
-      }
-    }
-
-    const chainChangedListener: Listener = (chainId: string) => {
-      setResetCount(resetCount + 1)
-      if (chainId) {
-        setChainId(parseInt(chainId))
-      } else {
-        setChainId(undefined)
-      }
-    }
-
-    if (provider && enabled) {
-      ethereum.on('accountsChanged', accountsChangedListener)
-      ethereum.on('chainChanged', chainChangedListener)
-    }
-
-    return () => {
-      if (window.ethereum) {
-        ethereum.off('accountsChanged', accountsChangedListener)
-        ethereum.off('chainChanged', chainChangedListener)
-      }
-    }
-  }, [provider, resetCount, enabled, ethereum])
-
   useEffect(() => {
     if (enabled) {
       const walletProvider = ethereum ? new Web3Provider(window.ethereum) : null
@@ -95,8 +48,10 @@ export const MetaMaskEthersLoader: React.FC<PropsWithChildren<Props>> = (props) 
       }
       setSigner(signer)
     }
-  }, [ethereum, isConnected, enabled])
+  }, [isConnected, enabled])
 
+  // Connectivity and Account / Address
+  const [localAddress, setLocalAddress] = useState<EthAddress>()
   useAsyncEffect(
     // eslint-disable-next-line react-hooks/exhaustive-deps
     async () => {
@@ -116,17 +71,69 @@ export const MetaMaskEthersLoader: React.FC<PropsWithChildren<Props>> = (props) 
     [signer, resetCount, enabled],
   )
 
+  const connect = useCallback(async () => {
+    const accounts = await walletProvider?.send('eth_requestAccounts', [])
+    // We could have multiple accounts. Check for one.
+    if (accounts.length !== 0) {
+      setLocalAddress(accounts[0])
+      console.log('Connected: ', accounts[0])
+    } else {
+      console.log('No authorized account found.')
+    }
+    return accounts
+  }, [walletProvider])
+
+  useEffect(() => {
+    const accountsChangedListener: Listener = (accounts: string[]) => {
+      setResetCount(resetCount + 1)
+      setError(undefined)
+      if (accounts.length > 0) {
+        setLocalAddress(EthAddress.fromString(accounts[0]))
+      } else {
+        setLocalAddress(undefined)
+      }
+    }
+
+    if (provider && enabled) {
+      ethereum.on('accountsChanged', accountsChangedListener)
+    }
+
+    return () => {
+      if (window.ethereum) {
+        ethereum.off('accountsChanged', accountsChangedListener)
+      }
+    }
+  }, [provider, resetCount, enabled])
+
+  // Chain Id
   const [chainId, setChainId] = useState<number>()
   useAsyncEffect(
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    async (isMounted) => {
-      if (enabled) {
-        const chainId = (await provider?.getNetwork())?.chainId
-        if (!isMounted()) return
-        setChainId(chainId)
+    async () => {
+      // try to grab chain id without connecting
+      const chainId = (await provider?.getNetwork())?.chainId
+      setChainId(chainId)
+
+      const chainChangedListener: Listener = (chainId: string) => {
+        setResetCount(resetCount + 1)
+        if (chainId) {
+          setChainId(parseInt(chainId))
+        } else {
+          setChainId(undefined)
+        }
+      }
+
+      if (enabled && ethereum) {
+        ethereum.on('chainChanged', chainChangedListener)
+      }
+
+      return () => {
+        if (ethereum) {
+          ethereum.off('chainChanged', chainChangedListener)
+        }
       }
     },
-    [provider, enabled],
+    [provider, enabled, resetCount],
   )
 
   return (
