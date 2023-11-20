a5555851997f8be585954a56a9ce6bf59e8af1a3
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-11-20 15:12:04 -0500

    use JsonRpcSigner instead of Signer

--- packages/crypto/src/contexts/Ethers/Context.ts
+++ packages/crypto/src/contexts/Ethers/Context.ts
@@ -1,6 +1,5 @@
-import { Signer } from '@ethersproject/abstract-signer'
-import { Provider } from '@ethersproject/providers'
 import { EthAddress } from '@xylabs/eth-address'
+import { JsonRpcSigner, Provider } from 'ethers'
 import { createContext } from 'react'
 
 export interface EthersData {
@@ -15,7 +14,7 @@ export interface EthersData {
   providerName?: string
   showConnectWalletDialog?: () => void
   signMessage?: (message: string, address?: string) => Promise<string | undefined>
-  signer?: Signer | null
+  signer?: JsonRpcSigner | null
   signerAddress?: string
   /** @deprecated - use the provider property and do not rely on wallet specific version */
   walletProvider?: Provider | null
