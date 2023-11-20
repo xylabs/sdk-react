36ad0ccf284583cd61a56fa959a538d0b263c1b2
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-11-20 12:43:06 -0500

    extract EthWalletConnector interface and implement in base

--- packages/crypto/src/contexts/Ethers/wallets/lib/EthWalletConnectorBase.ts
+++ packages/crypto/src/contexts/Ethers/wallets/lib/EthWalletConnectorBase.ts
@@ -4,10 +4,26 @@ import { EIP1193Provider } from './EIP1193'
 import { EIP1193Events } from './EIP1193Events'
 import { SupportedEventProposals } from './SupportedEvents'
 
+export interface EthWalletConnector {
+  allowedAddresses: string[]
+  chainId: number | undefined
+  installed: boolean
+  provider?: Web3Provider
+  providerName?: string
+  connectWallet(): Promise<void>
+  currentAddress(): Promise<string[] | undefined>
+  currentChainId(): Promise<string | number | null>
+  signMessage(message: string, address?: string): Promise<string | undefined>
+  signerFromAddress(address?: string): JsonRpcSigner
+  walletConnected(): Promise<boolean>
+}
+
 /**
  * Base class for connecting to an ethereum compatible wallet
  */
-export abstract class EthWalletConnectorBase extends EIP1193Events<EIP1193Provider> {
+export abstract class EthWalletConnectorBase extends EIP1193Events<EIP1193Provider> implements EthWalletConnector {
+  abstract allowedAddresses: string[]
+  abstract chainId: number | undefined
   abstract ethereum: unknown
   abstract provider: Web3Provider
   abstract providerName: string
@@ -21,6 +37,7 @@ export abstract class EthWalletConnectorBase extends EIP1193Events<EIP1193Provid
   abstract connectWallet(): Promise<void>
   abstract currentAddress(): Promise<string[] | undefined>
   abstract currentChainId(): Promise<string | number | null>
-  abstract signer(address?: string): JsonRpcSigner
+  abstract signMessage(message: string, address?: string): Promise<string | undefined>
+  abstract signerFromAddress(address?: string): JsonRpcSigner
   abstract walletConnected(): Promise<boolean>
 }
