a8589fc86ff1c4519c51d5be02ee2e69e97005c9
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-11-20 18:08:40 -0500

    remove interface and rely on abstract base for shape

--- packages/crypto/src/contexts/Ethers/wallets/EthWallet.ts
+++ packages/crypto/src/contexts/Ethers/wallets/EthWallet.ts
@@ -1,4 +1,4 @@
-import { BrowserProvider, JsonRpcSigner, Listener } from 'ethers'
+import { BrowserProvider } from 'ethers'
 
 /**
  * Base interface for wallet state and interaction
@@ -12,20 +12,4 @@ export interface EthWalletBase {
   connectWallet(): Promise<void>
   signMessage(message: string, address?: string): Promise<string | undefined>
   // TODO - transactions
-}
-
-/**
- * Connector interface for wallets implementations
- *
- * Extension of the base wallet functionality to add more provider specific methods
- */
-export interface EthWalletConnector extends EthWalletBase {
-  browserProviderOn?(event: string, listener: Listener): void
-  browserProviderRemoveListener?(event: string, listener: Listener): void
-  browserProviderRemoveListeners?(): void
-  currentAddress(): Promise<string[] | undefined>
-  currentChainId(): Promise<string | number | null>
-  signerFromAddress(address?: string): Promise<JsonRpcSigner>
-  subscribeToAddressChanges?(listener: () => void): () => void
-  subscribeToChainChanges?(listener: () => void): () => void
 }
