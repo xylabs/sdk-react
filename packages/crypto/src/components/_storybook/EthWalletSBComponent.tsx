2ccdd20f59ed2bea7a9ec7ec6f8da587f4ff8ab9
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-11-21 18:08:58 -0500

    optional iframe warning

--- packages/crypto/src/components/_storybook/EthWalletSBComponent.tsx
+++ packages/crypto/src/components/_storybook/EthWalletSBComponent.tsx
@@ -1,16 +1,22 @@
+import { OpenInNewOutlined } from '@mui/icons-material'
 import { Alert, AlertTitle, Button, List, ListItem, Typography } from '@mui/material'
 import { FlexCol, FlexRow } from '@xylabs/react-flexbox'
 import { useMemo, useState } from 'react'
 
 import { EthWallet } from '../../wallets'
 
-export const EthWalletSBComponent: React.FC<EthWallet> = ({
+export interface EthWalletSBComponentProps extends EthWallet {
+  noIFrames?: boolean
+}
+
+export const EthWalletSBComponent: React.FC<EthWalletSBComponentProps> = ({
   connectWallet,
   connectRefused,
   chainId,
   connectError,
   installed,
   currentAccount,
+  noIFrames,
   provider,
   providerName,
   signMessage,
@@ -27,35 +33,44 @@ export const EthWalletSBComponent: React.FC<EthWallet> = ({
   const localAddress = useMemo(() => currentAccount?.toString(), [currentAccount])
   return (
     <FlexCol alignItems="start" gap={2}>
-      {window.ethereum ? <Alert>Found window.ethereum</Alert> : null}
-      <FlexRow justifyContent="start" gap={2}>
-        <Button variant="contained" onClick={async () => await connectWallet?.()}>
-          Connect
-        </Button>
-        <Button disabled={!currentAccount} variant="contained" onClick={onSign}>
-          Sign
-        </Button>
-      </FlexRow>
-      {signResponse ? (
-        <Alert severity={'success'}>
-          <AlertTitle>Sign Response</AlertTitle>
-          {signResponse}
+      {noIFrames && window.parent !== window ? (
+        <Alert severity={'warning'}>
+          <AlertTitle>Must test outside of iframe</AlertTitle>
+          The {providerName} wallet does not allow the the permissions for to work from a nested iframe. Look for the <OpenInNewOutlined /> icon in
+          the upper right.
         </Alert>
       ) : null}
-      <Typography variant="h6" mb={0}>
-        Provider Details
-      </Typography>
-      <List sx={{ py: 0 }}>
-        <ListItem>Installed: {JSON.stringify(installed)}</ListItem>
-        <ListItem>Chain Id: {chainId}</ListItem>
-        <ListItem>Local Address: {localAddress?.toString()}</ListItem>
-        <ListItem>ProviderName: {providerName}</ListItem>
-        <ListItem>Provider: {JSON.stringify(!!provider)}</ListItem>
-        <ListItem>Signer: {JSON.stringify(!!signer)}</ListItem>
-        <ListItem>Signer Address: {signerAddress}</ListItem>
-        <ListItem>Connection Refused: {JSON.stringify(connectRefused)}</ListItem>
-        <ListItem>Error: {connectError?.message ?? connectError?.message}</ListItem>
-      </List>
+      <FlexCol alignItems="start" gap={2}>
+        {window.ethereum ? <Alert>Found window.ethereum</Alert> : null}
+        <FlexRow justifyContent="start" gap={2}>
+          <Button variant="contained" onClick={async () => await connectWallet?.()}>
+            Connect
+          </Button>
+          <Button disabled={!currentAccount} variant="contained" onClick={onSign}>
+            Sign
+          </Button>
+        </FlexRow>
+        {signResponse ? (
+          <Alert severity={'success'}>
+            <AlertTitle>Sign Response</AlertTitle>
+            {signResponse}
+          </Alert>
+        ) : null}
+        <Typography variant="h6" mb={0}>
+          Provider Details
+        </Typography>
+        <List sx={{ py: 0 }}>
+          <ListItem>Installed: {JSON.stringify(installed)}</ListItem>
+          <ListItem>Chain Id: {chainId}</ListItem>
+          <ListItem>Local Address: {localAddress?.toString()}</ListItem>
+          <ListItem>ProviderName: {providerName}</ListItem>
+          <ListItem>Provider: {JSON.stringify(!!provider)}</ListItem>
+          <ListItem>Signer: {JSON.stringify(!!signer)}</ListItem>
+          <ListItem>Signer Address: {signerAddress}</ListItem>
+          <ListItem>Connection Refused: {JSON.stringify(connectRefused)}</ListItem>
+          <ListItem>Error: {connectError?.message ?? connectError?.message}</ListItem>
+        </List>
+      </FlexCol>
     </FlexCol>
   )
 }
