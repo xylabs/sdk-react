0406702a4b5c3a2c49abc2274a3c7811e8ca7f07
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-11-21 12:24:42 -0500

    working coinbase wallet story

---
+++ packages/crypto/src/wallets/third-party/Coinbase/Coinbase.stories.tsx
@@ -0,0 +1,29 @@
+import { Meta } from '@storybook/react'
+
+import { EthWalletSBComponent } from '../../../components'
+import { useCoinbaseWallet } from './use'
+
+const UseCoinbaseSBComponent = () => {
+  const hookState = useCoinbaseWallet()
+  return <EthWalletSBComponent {...hookState} />
+}
+
+const StorybookEntry = {
+  argTypes: {},
+  component: UseCoinbaseSBComponent,
+  parameters: {
+    docs: {
+      page: null,
+    },
+  },
+  title: 'wallets/third-party/Coinbase',
+} as Meta<typeof UseCoinbaseSBComponent>
+
+const Template = () => <UseCoinbaseSBComponent />
+
+const Default = Template.bind({})
+
+export { Default }
+
+// eslint-disable-next-line import/no-default-export
+export default StorybookEntry
