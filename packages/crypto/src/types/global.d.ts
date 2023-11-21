f50889dbd8fd65900be61b28f0e524850cdcac84
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-11-21 18:09:09 -0500

    phantom window type

--- packages/crypto/src/types/global.d.ts
+++ packages/crypto/src/types/global.d.ts
@@ -4,5 +4,10 @@ declare global {
   interface Window {
     // For easily passing to Ether's BrowserProvider which takes an Eip11993Provider
     ethereum?: Eip1193Provider
+    phantom?: {
+      ethereum: {
+        isPhantom: boolean
+      }
+    }
   }
 }
