28b92185976cde3c539cc03558607028dac5bc87
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-11-27 13:43:48 -0500

    hook for discovering wallets

---
+++ packages/crypto/src/wallets/components/Discovery/hooks/use.tsx
@@ -0,0 +1,44 @@
+import { BrowserProvider } from 'ethers'
+import { useEffect, useState } from 'react'
+
+import { EIP6963ProviderInfo, isEIP6963AnnounceProviderEvent } from '../lib'
+
+interface DiscoveredWallets {
+  [rdns: string]: {
+    info: EIP6963ProviderInfo
+    provider: BrowserProvider
+  }
+}
+
+export const useWalletDiscovery = () => {
+  const [discoveredWallets, setDiscoveredWallets] = useState<DiscoveredWallets>({})
+
+  useEffect(() => {
+    const listener = (event: Event) => {
+      if (isEIP6963AnnounceProviderEvent(event)) {
+        const { info, provider } = event.detail
+        // capture installed wallets as they come in
+        setDiscoveredWallets((previous) => ({
+          [info.rdns]: {
+            info,
+            provider,
+          },
+          ...previous,
+        }))
+      }
+    }
+
+    // listen when providers announce themselves
+    window.addEventListener('eip6963:announceProvider', listener)
+
+    // dispatch an event to ask all installed providers to identify themselves
+    window.dispatchEvent(new Event('eip6963:requestProvider'))
+
+    return () => {
+      // clean up listener
+      window.removeEventListener('eip6963:announceProvider', listener)
+    }
+  }, [])
+
+  return discoveredWallets
+}
---
+++ packages/crypto/src/wallets/components/Discovery/lib/EIP6963.ts
@@ -0,0 +1,37 @@
+import { BrowserProvider } from 'ethers'
+
+/**
+ * Assets needed to display a wallet
+ */
+export interface EIP6963ProviderInfo {
+  icon: string
+  name: string
+  rdns: string
+  uuid: string
+}
+
+/**
+ * Detail of the CustomEvent emitted by as wallet
+ */
+export interface EIP6963ProviderDetail {
+  info: EIP6963ProviderInfo
+  provider: BrowserProvider
+}
+
+/**
+ * CustomEvent emitted by a wallet
+ */
+export interface EIP6963AnnounceProviderEvent extends CustomEvent {
+  detail: EIP6963ProviderDetail
+  type: 'eip6963:announceProvider'
+}
+
+/**
+ * TypeGuard to inspect an event and know its of type EIP6963AnnounceProviderEvent
+ * @param event Event
+ * @returns boolean
+ */
+export const isEIP6963AnnounceProviderEvent = (event: Event): event is EIP6963AnnounceProviderEvent => {
+  const castEvent = event as EIP6963AnnounceProviderEvent
+  return !!castEvent.detail && !!castEvent.detail.info && !!castEvent.detail.provider
+}
