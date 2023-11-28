ce3ce5decd980ecfe1c0c7bbe367c59c507613ce
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-11-27 20:49:33 -0500

    code comment

--- packages/crypto/src/wallets/third-party/EIP6963/EIP6963Connector.ts
+++ packages/crypto/src/wallets/third-party/EIP6963/EIP6963Connector.ts
@@ -3,6 +3,11 @@ import { BrowserProvider, Eip1193Provider } from 'ethers'
 import { EthWalletConnectorBase } from '../../EthWalletConnectorBase'
 import { EIP6963ProviderInfo } from '../../lib'
 
+/**
+ * A generic connector for EIP-6963 compatible wallets
+ *
+ * see - https://eips.ethereum.org/EIPS/eip-6963
+ */
 export class EIP6963Connector extends EthWalletConnectorBase {
   // Name of the Provider
   public providerName = ''
