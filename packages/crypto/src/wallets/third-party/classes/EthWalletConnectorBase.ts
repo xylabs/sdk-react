e273877d2a71a8cdf26126aaec532a86c920715f
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-12-01 16:30:26 -0500

    single method for notifying subscribers

--- packages/crypto/src/wallets/third-party/classes/EthWalletConnectorBase.ts
+++ packages/crypto/src/wallets/third-party/classes/EthWalletConnectorBase.ts
@@ -1,6 +1,6 @@
 import { BrowserProvider, Eip1193Provider, Listener } from 'ethers'
 
-import { AccountsChangedEventDetails, AccountsChangedEventName, ChainChangedEventDetails, ChainChangedEventName } from '../../events'
+import { AccountsChangedEventName, ChainChangedEventName } from '../../events'
 import { EIP1193Events, EIP6963ProviderInfo, SupportedEventProposals } from '../../lib'
 import { findChainName } from '../../utils'
 
@@ -63,10 +63,6 @@ export abstract class EthWalletConnectorBase extends EIP1193Events {
     return await this.provider?.send('net_version', [])
   }
 
-  emitEvent(eventName: string, customEvent: CustomEventInit) {
-    window.dispatchEvent(new CustomEvent(eventName, customEvent))
-  }
-
   async signMessage(message: string, allowedAccounts?: string) {
     if (!this.provider) {
       this.logProviderMissing()
@@ -102,17 +98,13 @@ export abstract class EthWalletConnectorBase extends EIP1193Events {
   protected async onAccountsChangedListener() {
     // set the initial value
     this.allowedAccounts = (await this.currentAccounts()) ?? []
-    // notify existing subscribers that the default was set
-    this.accountChangeNotifiers.forEach((listener) => listener())
+
+    this.notifySubscribers(this.accountChangeNotifiers, AccountsChangedEventName, { allowedAccounts: this.allowedAccounts })
 
     const listener = (accounts: string[]) => {
       this.allowedAccounts = accounts
-      this.accountChangeNotifiers.forEach((listener) => listener())
 
-      const eventDetails: AccountsChangedEventDetails = {
-        detail: { allowedAccounts: accounts, providerName: this.providerName },
-      }
-      this.emitEvent(AccountsChangedEventName, eventDetails)
+      this.notifySubscribers(this.accountChangeNotifiers, AccountsChangedEventName, { allowedAccounts: this.allowedAccounts })
     }
     this.onAccountsChanged(listener)
   }
@@ -123,23 +115,30 @@ export abstract class EthWalletConnectorBase extends EIP1193Events {
   protected async onChainChangedListener() {
     // set the initial value
     this.chainIdHex = (await this.currentChainId()) ?? undefined
-    // notify existing subscribers that the default was set
-    this.chainChangedNotifiers.forEach((listener) => listener())
+
+    this.notifySubscribers(this.chainChangedNotifiers, ChainChangedEventName, { chainId: this.chainId })
 
     const listener = (chainId: string | undefined) => {
       this.chainIdHex = chainId
-      this.chainChangedNotifiers.forEach((listener) => listener())
 
-      const eventDetails: ChainChangedEventDetails = {
-        detail: { chainId: this.chainId, providerName: this.providerName },
-      }
-      this.emitEvent(ChainChangedEventName, eventDetails)
+      this.notifySubscribers(this.chainChangedNotifiers, ChainChangedEventName, { chainId: this.chainId })
     }
     this.onChainChanged(listener)
   }
 
   private logProviderMissing() {
     console.warn('Cannot call this method because there is no browser provider connected.  Please confirm that metamask is installed')
+  }
+
+  private notifySubscribers(listeners: Listener[], eventName: string, value: { [key: string]: unknown }) {
+    listeners.forEach((listener) => listener())
+
+    const details = {
+      detail: { ...value, providerName: this.providerName },
+    }
+
+    // Allow anyone to listen for changes
+    window.dispatchEvent(new CustomEvent(eventName, details))
   }
 
   // Since init likely relies on derived class members, derived classes must implement it
