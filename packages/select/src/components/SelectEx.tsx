42ce678f155e94c95058ce259d06232e8a934522
Arie Trouw <arie.trouw@xylabs.com>
2024-03-20 18:04:38 -0700

    SelectEx types fixed with new version of MUI

--- packages/select/src/components/SelectEx.tsx
+++ packages/select/src/components/SelectEx.tsx
@@ -1,12 +1,11 @@
-import { PaletteMode, Select, SelectProps, SelectVariants, useTheme } from '@mui/material'
+import { PaletteMode, Select, SelectProps, useTheme } from '@mui/material'
 import { merge } from '@xylabs/lodash'
 import React from 'react'
 
-export type SelectExProps<T, V extends SelectVariants = SelectVariants> = Omit<SelectProps<T, V>, 'variant'> &
-  Partial<SelectProps<T, V>> & {
-    colorize?: 'primary' | 'secondary'
-    mode?: PaletteMode
-  }
+export type SelectExProps<T> = SelectProps<T> & {
+  colorize?: 'primary' | 'secondary'
+  mode?: PaletteMode
+}
 
 export const SelectEx: <T>(props: SelectExProps<T>) => React.JSX.Element = ({ MenuProps, mode = 'light', colorize, ...props }) => {
   const theme = useTheme()
