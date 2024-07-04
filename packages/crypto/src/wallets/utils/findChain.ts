c45fd0eae43cd73544c2d73ce4c6f5f0a61f5e93
Arie Trouw <arie.trouw@xylabs.com>
2024-07-03 17:20:32 -0700

    updo and switched to viem for chain list

--- packages/crypto/src/wallets/utils/findChain.ts
+++ packages/crypto/src/wallets/utils/findChain.ts
@@ -1,3 +1,4 @@
-import * as chains from '@wagmi/chains'
+// eslint-disable-next-line import/no-internal-modules
+import chains from 'viem/chains'
 
 export const findChainName = (chainId: number) => Object.values(chains).find((chain) => chain.id === chainId)
