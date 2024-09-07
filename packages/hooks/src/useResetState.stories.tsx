809e001f5bbb15a81ef91bed780db31b89f8aad4
Arie Trouw <arie.trouw@xylabs.com>
2024-09-07 13:09:07 -0700

    deploy 4.2.10

---
+++ packages/hooks/src/useResetState.stories.tsx
@@ -0,0 +1,80 @@
+import type { Meta, StoryFn } from '@storybook/react'
+import type { FC } from 'react'
+import React, { useState } from 'react'
+
+import { useResetState } from './useResetState.ts'
+
+const Inner: FC<{ outerValue?: boolean }> = ({ outerValue }) => {
+  const [value, setValue] = useResetState(outerValue)
+  return (
+    <div style={{ border: 'solid 1px blue' }}>
+      <div>Inner</div>
+      <div>
+        Outer Value:
+        {outerValue?.toString()}
+      </div>
+      <div>
+        Inner Value:
+        {value?.toString()}
+      </div>
+      <button type="button" onClick={() => setValue(prev => !prev)}>
+        Toggle
+      </button>
+      <button type="button" onClick={() => setValue(true)}>
+        True
+      </button>
+      <button type="button" onClick={() => setValue(false)}>
+        False
+      </button>
+      <button type="button" onClick={() => setValue(undefined)}>
+        undefined
+      </button>
+    </div>
+  )
+}
+
+const Outer: FC = () => {
+  const [outerValue, setOuterValue] = useState<boolean>()
+  return (
+    <div style={{ border: 'solid 1px yellow' }}>
+      <div>Outer</div>
+      <Inner outerValue={outerValue} />
+      <div>
+        <div>
+          Outer Value:
+          {outerValue?.toString()}
+        </div>
+      </div>
+      <div>
+        <button type="button" onClick={() => setOuterValue(prev => !prev)}>
+          Toggle
+        </button>
+        <button type="button" onClick={() => setOuterValue(true)}>
+          True
+        </button>
+        <button type="button" onClick={() => setOuterValue(false)}>
+          False
+        </button>
+        <button type="button" onClick={() => setOuterValue(undefined)}>
+          undefined
+        </button>
+      </div>
+    </div>
+  )
+}
+
+const StorybookEntry = {
+  argTypes: {},
+  component: Outer,
+  parameters: { docs: { page: null } },
+  title: 'hooks/useResetState',
+} as Meta<typeof Outer>
+
+const Template: StoryFn<typeof Outer> = () => <Outer></Outer>
+
+const Default = Template.bind({})
+Default.args = { title: 'Empty' }
+
+export { Default }
+
+export default StorybookEntry
