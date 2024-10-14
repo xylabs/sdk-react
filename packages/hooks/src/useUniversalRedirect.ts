dbc993e485a6665ec87c39c9ecd691949c99653c
Arie Trouw <arie.trouw@xylabs.com>
2024-10-14 11:44:00 -0700

    name type forced to string

--- packages/hooks/src/useUniversalRedirect.ts
+++ packages/hooks/src/useUniversalRedirect.ts
@@ -2,14 +2,14 @@ import { useLocation, useNavigate } from 'react-router-dom'
 
 const DEFAULT_NAME = 'universalRedirect|to' as const
 
-export const useSetUniversalRedirect = (name = DEFAULT_NAME) => {
+export const useSetUniversalRedirect = (name: string = DEFAULT_NAME) => {
   const location = useLocation()
   return (to?: string) => {
     localStorage.setItem(name, to ?? location.pathname)
   }
 }
 
-export const useCheckUniversalRedirect = (name = DEFAULT_NAME) => {
+export const useCheckUniversalRedirect = (name: string = DEFAULT_NAME) => {
   const navigate = useNavigate()
 
   const pendingTo = localStorage.getItem(name)
