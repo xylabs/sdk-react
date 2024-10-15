8aadaab80be8c3f6ca1cadcfe99782e996d28113
Arie Trouw <arie.trouw@xylabs.com>
2024-10-15 16:47:43 -0700

    Link no href & to props and Button update

--- packages/button/src/components/ButtonExProps.tsx
+++ packages/button/src/components/ButtonExProps.tsx
@@ -2,6 +2,39 @@ import type { ButtonProps } from '@mui/material'
 import type { BoxlikeComponentProps, BusyProps } from '@xylabs/react-shared'
 import type { NavigateOptions, To } from 'react-router-dom'
 
+export interface ButtonOnlyHrefProps {
+  href?: string
+  to?: never
+  toOptions?: never
+}
+
+export interface ButtonOnlyToProps {
+  href?: never
+  to?: To
+  toOptions?: NavigateOptions
+}
+
+export interface ButtonNoToOrHrefProps {
+  href?: never
+  to?: never
+  toOptions?: never
+}
+
+export type ButtonHrefOrToOrNoProps = ButtonOnlyHrefProps | ButtonOnlyToProps | ButtonNoToOrHrefProps
+
+export interface ButtonHrefAndToProps {
+  href?: string
+  to?: To
+  toOptions?: NavigateOptions
+}
+
+export const asButtonHrefOrToProps = (props: ButtonHrefAndToProps): ButtonHrefOrToOrNoProps => {
+  if (props.href && (props.to || props.toOptions)) {
+    throw new Error('ButtonExProps: cannot have both href and to')
+  }
+  return props.href ? { href: props.href } : props.to ? { to: props.to, toOptions: props.toOptions } : {}
+}
+
 export interface ButtonBaseExProps extends Omit<ButtonProps, 'ref' | 'href'>, BoxlikeComponentProps, BusyProps {
   disableUserEvents?: boolean
   funnel?: string
@@ -10,22 +43,4 @@ export interface ButtonBaseExProps extends Omit<ButtonProps, 'ref' | 'href'>, Bo
   target?: string
 }
 
-export interface ButtonToExProps extends ButtonBaseExProps {
-  href?: never
-  to: To
-  toOptions?: NavigateOptions
-}
-
-export interface ButtonHrefExProps extends ButtonBaseExProps {
-  href: string
-  to?: never
-  toOptions?: never
-}
-
-export interface ButtonClickExProps extends ButtonBaseExProps {
-  href?: never
-  to?: never
-  toOptions?: never
-}
-
-export type ButtonExProps = ButtonToExProps | ButtonHrefExProps | ButtonClickExProps
+export type ButtonExProps = ButtonBaseExProps & ButtonHrefOrToOrNoProps
--- packages/link/src/LinkExProps.tsx
+++ packages/link/src/LinkExProps.tsx
@@ -1,11 +1,44 @@
 import type { LinkProps } from '@mui/material'
 import type { NavigateOptions, To } from 'react-router-dom'
 
-export interface LinkExProps extends Omit<LinkProps, 'ref'> {
+export interface LinkOnlyHrefProps {
+  href?: string
+  to?: never
+  toOptions?: never
+}
+
+export interface LinkOnlyToProps {
+  href?: never
+  to?: To
+  toOptions?: NavigateOptions
+}
+
+export interface LinkNoToOrHrefProps {
+  href?: never
+  to?: never
+  toOptions?: never
+}
+
+export type LinkHrefOrToOrNoProps = LinkOnlyHrefProps | LinkOnlyToProps | LinkNoToOrHrefProps
+
+export interface LinkHrefAndToProps {
+  href?: string
+  to?: To
+  toOptions?: NavigateOptions
+}
+
+export const asLinkHrefOrToProps = (props: LinkHrefAndToProps): LinkHrefOrToOrNoProps => {
+  if (props.href && (props.to || props.toOptions)) {
+    throw new Error('LinkExProps: cannot have both href and to')
+  }
+  return props.href ? { href: props.href } : props.to ? { to: props.to, toOptions: props.toOptions } : {}
+}
+
+export interface LinkExBaseProps extends Omit<LinkProps, 'ref' | 'href'> {
   disableUserEvents?: boolean
   funnel?: string
   intent?: string
   placement?: string
-  to?: To
-  toOptions?: NavigateOptions
 }
+
+export type LinkExProps = LinkExBaseProps & LinkHrefOrToOrNoProps
