e1d7e522dcacdfdfbdd89016f7da3b6ca2e2dd7a
Arie Trouw <arie.trouw@xylabs.com>
2024-11-01 16:19:52 -0700

    toOptions error fixed in LinkEx
    5.2.16

--- packages/link/src/LinkToEx.tsx
+++ packages/link/src/LinkToEx.tsx
@@ -2,7 +2,7 @@ import { Link } from '@mui/material'
 import { useUserEvents } from '@xylabs/react-pixel'
 import type { MouseEvent } from 'react'
 import React, { forwardRef } from 'react'
-import { Link as RouterLink } from 'react-router-dom'
+import { Link as RouterLink, useNavigate } from 'react-router-dom'
 
 import type { LinkExProps } from './LinkExProps.tsx'
 
@@ -10,28 +10,39 @@ export const LinkToEx = forwardRef<HTMLAnchorElement, LinkExProps>(({
   intent,
   funnel,
   onClick,
+  to,
+  toOptions,
   placement,
   disableUserEvents,
   ...props
 }, ref) => {
   const userEvents = useUserEvents()
+  const navigate = useNavigate()
   const localOnClick = (event: MouseEvent<HTMLAnchorElement>) => {
     if (!disableUserEvents && userEvents) {
       const elementName = props['aria-label'] ?? event.currentTarget.textContent
+      const callOnClickAndNavigate = () => {
+        try {
+          onClick?.(event)
+        } catch (ex) {
+          console.error('onClick threw', elementName, funnel, placement, ex)
+        }
+        if (to && toOptions) {
+          navigate(to, toOptions)
+        }
+      }
       userEvents.userClick({
         elementName, funnel, intent, placement,
-      }).then(() => {
-        onClick?.(event)
-      }).catch((ex) => {
+      }).then(callOnClickAndNavigate).catch((ex) => {
         console.error('User event failed', elementName, funnel, placement, ex)
-        onClick?.(event)
+        callOnClickAndNavigate()
       })
     } else {
       onClick?.(event)
     }
   }
 
-  return <Link ref={ref} rel="noopener noreferrer" component={RouterLink} onClick={localOnClick} {...props} />
+  return <Link ref={ref} rel="noopener noreferrer" to={toOptions ? to : undefined} component={RouterLink} onClick={localOnClick} {...props} />
 })
 
 LinkToEx.displayName = 'LinkToExXYLabs'
