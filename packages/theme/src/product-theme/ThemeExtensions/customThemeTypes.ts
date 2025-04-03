0f5bd222ec5b53b089f9773d38a45a953602bd7e
Arie Trouw <arie.trouw@xylabs.com>
2025-04-03 13:31:06 -0700

    Extends MUI theme with neutral color and IconButton
    Extends the Material UI theme to include a 'neutral' color option in the palette and IconButton.
    
    This allows for a more consistent and flexible color scheme across the application,
    particularly for UI elements requiring a neutral tone.

--- packages/theme/src/product-theme/ThemeExtensions/customThemeTypes.ts
+++ packages/theme/src/product-theme/ThemeExtensions/customThemeTypes.ts
@@ -1,14 +1,5 @@
 import type { PaletteColorOptions } from '@mui/material/styles'
 
-declare module '@mui/material' {
-  interface PaletteOptions {
-    neutral?: PaletteColorOptions
-  }
-  interface TypeBackground {
-    gradient: string
-  }
-}
-
 declare module '@mui/material/styles' {
   interface PaletteOptions {
     neutral?: PaletteColorOptions
@@ -26,6 +17,12 @@ declare module '@mui/material/Button' {
 
 declare module '@mui/material/Alert' {
   interface AlertPropsColorOverrides {
+    neutral: true
+  }
+}
+
+declare module '@mui/material/IconButton' {
+  interface IconButtonPropsColorOverrides {
     neutral: true
   }
 }
