a029563c256ef7ee1550a4954b9b94cfc7f4b651
Matt Jones <885417+jonesmac@users.noreply.github.com>
2025-04-29 16:31:52 -0400

    hide error details by default

--- packages/error/src/components/ErrorRender/ErrorAlert.tsx
+++ packages/error/src/components/ErrorRender/ErrorAlert.tsx
@@ -1,10 +1,11 @@
-import { ExitToApp as ExitIcon } from '@mui/icons-material'
+import { ChevronRight, ExitToApp as ExitIcon } from '@mui/icons-material'
 import type { AlertProps } from '@mui/material'
 import {
-  Alert, AlertTitle, Typography,
+  Alert, AlertTitle, Chip, Collapse, Grow, Stack, Typography,
 } from '@mui/material'
 import { ButtonEx } from '@xylabs/react-button'
-import React from 'react'
+import { FlexCol } from '@xylabs/react-flexbox'
+import React, { useState } from 'react'
 
 export interface ErrorAlertProps<T = void> extends AlertProps {
   additionalMessaging?: React.ReactNode
@@ -22,25 +23,43 @@ export function ErrorAlert<T = void>({
   scope,
   ...props
 }: ErrorAlertProps<T>): React.JSX.Element {
+  const [openDetails, setOpenDetails] = useState(false)
   return (
     <Alert action={action} severity="error" {...props}>
       <AlertTitle>{title}</AlertTitle>
-      {scope
-        ? (
+      <FlexCol width="100%" alignItems="start" gap={1}>
+        {additionalMessaging}
+        <Chip
+          avatar={openDetails ? <ChevronRight sx={{ transform: 'rotate(90deg)' }} /> : <ChevronRight />}
+          label="Error Details"
+          onClick={() => setOpenDetails(!openDetails)}
+          sx={{ mt: 1 }}
+          variant="outlined"
+        />
+        <Grow in={openDetails} unmountOnExit>
+          <Stack sx={{
+            border: '1px solid', borderColor: 'divider', borderRadius: 1, padding: 1, mt: 1,
+          }}
+          >
+            {scope
+              ? (
+                  <div>
+                    <Typography variant="caption" mr={0.5} fontWeight="bold">
+                      Scope:
+                    </Typography>
+                    <Typography variant="caption">{scope}</Typography>
+                  </div>
+                )
+              : null}
             <div>
               <Typography variant="caption" mr={0.5} fontWeight="bold">
-                Scope:
+                Error:
               </Typography>
-              <Typography variant="caption">{scope}</Typography>
+              <Typography variant="caption">{typeof error === 'string' ? error : (error as Error)?.message}</Typography>
             </div>
-          )
-        : null}
-      <div>
-        <Typography variant="caption" mr={0.5} fontWeight="bold">
-          Error:
-        </Typography>
-        <Typography variant="caption">{typeof error === 'string' ? error : (error as Error)?.message}</Typography>
-      </div>
+          </Stack>
+        </Grow>
+      </FlexCol>
       {onCancel
         ? (
             <ButtonEx
@@ -54,7 +73,6 @@ export function ErrorAlert<T = void>({
             </ButtonEx>
           )
         : null}
-      {additionalMessaging}
     </Alert>
   )
 }
