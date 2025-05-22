fc6eb78e7e120b63d4425a83bbc72e57df922d3f
Matt Jones <885417+jonesmac@users.noreply.github.com>
2025-05-22 11:47:24 -0400

    formatting

--- packages/theme/src/alphaCss.ts
+++ packages/theme/src/alphaCss.ts
@@ -4,8 +4,8 @@ export const alphaCss = (color: string, value: number) => {
    * we need to actually apply the opacity value to the color.
    * That way, the transparent value will be the remaining percentage.
    *
-   * i.e. when value = 0.25 then 25% of the color is applied, and 100% - 25% (75%) of transparent
-   * is applied.
+   * i.e. when value = 0.25 then 25% of the color is applied, and 100% - 25% (75%)
+   * of transparent is applied.
    */
   return `color-mix(in srgb, ${color} ${value * 100}%, transparent)`
 }
