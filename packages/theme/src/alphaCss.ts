eed27d8ec4ee8b444a0449b7e693abe605560d9e
Matt Jones <885417+jonesmac@users.noreply.github.com>
2025-05-22 11:46:37 -0400

    documentation around color-mix usage

--- packages/theme/src/alphaCss.ts
+++ packages/theme/src/alphaCss.ts
@@ -1,3 +1,11 @@
 export const alphaCss = (color: string, value: number) => {
+  /**
+   * Since we want to mimic the behavior of the alpha function in mui,
+   * we need to actually apply the opacity value to the color.
+   * That way, the transparent value will be the remaining percentage.
+   *
+   * i.e. when value = 0.25 then 25% of the color is applied, and 100% - 25% (75%) of transparent
+   * is applied.
+   */
   return `color-mix(in srgb, ${color} ${value * 100}%, transparent)`
 }
