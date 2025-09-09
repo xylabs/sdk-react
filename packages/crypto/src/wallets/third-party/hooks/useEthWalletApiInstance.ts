1ce653ea937e8d9c07c86ac0b6be16dc73c5a8fe
Matt Jones <885417+jonesmac@users.noreply.github.com>
2025-09-09 13:41:30 -0400

    hook rename and composure

---
+++ packages/crypto/src/wallets/third-party/hooks/useEthWalletApiInstance.ts
@@ -0,0 +1,18 @@
+import { useMemo } from 'react'
+
+import { EthWalletApiInstance } from '../../lib/index.ts'
+import type { EthWalletConnectorBase } from '../classes/index.ts'
+
+/**
+ * A wrapper for the Ethereum wallet connection that adds functionality that is not
+ * supported by the provider interface directly.
+ *
+ * Metamask in particular supports many `wallet_*` rpc calls have their own EIP-* proposal.
+ * See - https://docs.metamask.io/wallet/reference/json-rpc-methods/
+ *
+ * @param connector An instance of EthWalletConnectorBase
+ * @returns
+ */
+export const useEthWalletApiInstance = (connector: EthWalletConnectorBase) => {
+  return useMemo(() => new EthWalletApiInstance(connector), [connector])
+}
--- packages/crypto/src/wallets/third-party/hooks/useEthWalletInstance.ts
+++ packages/crypto/src/wallets/third-party/hooks/useEthWalletInstance.ts
@@ -1,20 +1,13 @@
-import { useMemo } from 'react'
-
-import { EthWalletInstance } from '../../lib/index.ts'
 import type { EthWalletConnectorBase } from '../classes/index.ts'
+import { useEthWallet } from './useEthWallet.tsx'
+import { useEthWalletApiInstance } from './useEthWalletApiInstance.ts'
 
-/**
- * A wrapper for the Ethereum wallet connection that adds functionality that is not
- * supported by the provider interface directly.
- *
- * Metamask in particular supports many `wallet_*` rpc calls have their own EIP-* proposal.
- * See - https://docs.metamask.io/wallet/reference/json-rpc-methods/
- *
- * @param connector An instance of EthWalletConnectorBase
- * @returns
- */
 export const useEthWalletInstance = (connector: EthWalletConnectorBase) => {
-  const ethWalletInstance = useMemo(() => new EthWalletInstance(connector), [connector])
+  const ethWalletApiInstance = useEthWalletApiInstance(connector)
+  const ethWallet = useEthWallet(connector)
 
-  return ethWalletInstance
+  return {
+    ethWalletApiInstance,
+    ethWallet,
+  }
 }
