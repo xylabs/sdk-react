34a8d6b9c038500d20503e7010bd10ed24a18f12
Matt Jones <885417+jonesmac@users.noreply.github.com>
2025-09-10 17:59:06 -0400

    fix ethers imports and associated lint rules

--- packages/crypto/src/wallets/classes/EIP1193Events.ts
+++ packages/crypto/src/wallets/classes/EIP1193Events.ts
@@ -1,7 +1,6 @@
 import { forget } from '@xylabs/forget'
-import type {
-  BrowserProvider, Eip1193Provider, Listener,
-} from 'ethers'
+import type { BrowserProvider, Eip1193Provider } from 'ethers/providers'
+import type { Listener } from 'ethers/utils'
 
 import type {
   EIP1193EventNames, EIP1193EventsCompatible, SupportedEventProposals,
