290e11c55e6e99ea347ce3a225a33e880fecade8
Matt Jones <885417+jonesmac@users.noreply.github.com>
2025-09-10 14:44:21 -0400

    document and use provider from connector base

--- packages/crypto/src/wallets/lib/EthWalletApiInstance.ts
+++ packages/crypto/src/wallets/lib/EthWalletApiInstance.ts
@@ -1,9 +1,19 @@
 import { assertEx } from '@xylabs/assert'
 import type { Hex } from '@xylabs/hex'
+import type { EIP1193Provider } from 'viem'
 
 import type { EthWalletConnectorBase } from '../third-party/index.ts'
 import type { EthWalletApi } from '../types/index.ts'
 
+/**
+ * A wrapper for the Ethereum wallet connection that adds functionality that is not
+ * supported by the provider interface directly.
+ *
+ * Metamask in particular supports many `wallet_*` rpc calls have their own EIP-* proposal.
+ * See - https://docs.metamask.io/wallet/reference/json-rpc-methods/
+ *
+ * @param connector An instance of EthWalletConnectorBase
+ */
 export class EthWalletApiInstance implements EthWalletApi {
   private _ethWalletConnector: EthWalletConnectorBase
 
@@ -11,15 +21,22 @@ export class EthWalletApiInstance implements EthWalletApi {
     this._ethWalletConnector = ethWalletConnector
   }
 
+  private get eip1193Provider(): EIP1193Provider {
+    return assertEx('request' in (this.ethWalletConnector.provider || {})
+      ? this.ethWalletConnector.provider as unknown as EIP1193Provider
+      : null, () => 'Provider does not support request method')
+  }
+
   private get ethWalletConnector() {
     return this._ethWalletConnector
   }
 
-  async switchEthereumChain(chainId?: Hex) {
-    const walletChainId = assertEx(this.ethWalletConnector.chainId, () => 'No chainId connected')
-    if (walletChainId !== Number(chainId)) {
-      const eip1193Provider = assertEx(globalThis.ethereum, () => 'No ethereum provider found on window')
-      await eip1193Provider.request({
+  async switchEthereumChain(chainId: Hex) {
+    assertEx(this.ethWalletConnector.chainId, () => 'No chainId connected')
+    assertEx(chainId, () => 'No chainId provided')
+
+    if (this.ethWalletConnector.chainId !== Number(chainId)) {
+      await this.eip1193Provider.request({
         method: 'wallet_switchEthereumChain',
         params: [{ chainId }],
       })
