ebf2e086068d1562dc90886a23a664519685bb77
Matt Jones <885417+jonesmac@users.noreply.github.com>
2025-09-10 14:36:52 -0400

    fold proxy trap logic into hook

--- packages/crypto/src/hooks/useTryMethodCalls.ts
+++ packages/crypto/src/hooks/useTryMethodCalls.ts
@@ -1,19 +1,26 @@
-export function catchMethodErrors<T extends object>(instance: T, onError: (error: Error) => void): T {
-  return new Proxy(instance, {
+import { useMemo, useState } from 'react'
+
+export const useTryMethodCalls = <T extends object>(instance: T): { error: Error | undefined; instance: T } => {
+  const [error, setError] = useState<Error>()
+
+  const proxy = useMemo(() => new Proxy(instance, {
     get(target, prop, receiver) {
       const orig = Reflect.get(target, prop, receiver)
       // Only wrap functions that are not private (do not start with _)
       if (typeof orig === 'function' && typeof prop === 'string' && !prop.startsWith('_')) {
+        setError(undefined)
         return async function (...args: unknown[]) {
           try {
             return await orig.apply(instance, args)
           } catch (err) {
-            onError(err as Error)
+            setError(err as Error)
             throw err
           }
         }
       }
       return orig
     },
-  })
+  }), [instance])
+
+  return { instance: proxy, error }
 }
--- packages/crypto/src/wallets/lib/index.ts
+++ packages/crypto/src/wallets/lib/index.ts
@@ -1,4 +1,3 @@
-export * from './catchMethodErrors.ts'
 export * from './EIP1193.ts'
 export * from './EIP1193Events.ts'
 export * from './EIP6963.ts'
