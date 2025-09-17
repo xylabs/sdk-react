ce5dbae950d276337ee8ddc0cf1dd013c24a25a2
Matt Jones <885417+jonesmac@users.noreply.github.com>
2025-09-17 12:21:12 -0400

    create a stable reference for getSnapShot

--- packages/crypto/src/wallets/third-party/hooks/useCurrentAccount.tsx
+++ packages/crypto/src/wallets/third-party/hooks/useCurrentAccount.tsx
@@ -4,13 +4,16 @@ import { useMemo, useSyncExternalStore } from 'react'
 
 import type { EthWalletConnectorBase } from '../classes/index.ts'
 
+// stable value for getSnapShot
+export const EMPTY_ARRAY = [] as const
+
 /**
  * Subscribe to account changes from a given wallet
  **/
 export const useCurrentAccountExternal = (ethWalletConnector?: EthWalletConnectorBase) => {
   const { getSnapShot, subscribe } = useMemo(() => {
     return {
-      getSnapShot: () => isDefined(ethWalletConnector) ? ethWalletConnector.allowedAccounts : [],
+      getSnapShot: () => isDefined(ethWalletConnector) ? ethWalletConnector.allowedAccounts : EMPTY_ARRAY,
       subscribe: (notifier: () => void) => isDefined(ethWalletConnector) ? ethWalletConnector.subscribeToAccountsChanges(notifier) : () => {},
     }
   }, [ethWalletConnector])
