9e4962f3c5ec504b12f79a2933c19d277f4b13d6
Matt Jones <885417+jonesmac@users.noreply.github.com>
2026-01-28 10:48:10 -0500

    remove unnecessary type fix

--- packages/theme/src/types/global.d.ts
+++ packages/theme/src/types/global.d.ts
@@ -1,19 +1,10 @@
 import 'react'
 import '@mui/material/themeCssVarsAugmentation'
 
-// NOTE:  Because @react-spring/web is not updated to support React 19, it relies on the
-// deprecated global JSX namespace to declare the intrinsic elements. This is a workaround
-// to suppress the typescript errors that result of that reliance. This workaround is only
-// to satisfy the type resolution and does not affect functionality.  Ideally, @react-spring/web
-// will update to react 19 or we should move away from it altogether to something like motion-js
-
 declare global {
   declare module '*.png'
   declare module '*.jpg'
   declare module '*.svg'
   declare module '*.gif'
   declare module '*.webp'
-  namespace JSX {
-    type IntrinsicElements = React.JSX.IntrinsicElements
-  }
 }
